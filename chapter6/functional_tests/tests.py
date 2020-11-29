cb3e6038263e85f5250991dd7726239a3786d89c
richard-nguyen22 <richard.ngt22@gmail.com>
2020-11-29 20:43:39 +0800

    Use Selenium WebDriverException for a explicit wait

--- chapter6/functional_tests/tests.py
+++ chapter6/functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10 # Maximum waiting time to catch glitchers or random slowness
 
 class NewVisitorTest(LiveServerTestCase):
   def setUp(self):
@@ -10,10 +13,18 @@ class NewVisitorTest(LiveServerTestCase):
   def tearDown(self):
     self.browser.quit()
 
-  def check_for_row_in_list_table(self, row_text):
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn(row_text, [row.text for row in rows])
+  def wait_for_row_in_list_table(self, row_text):
+    start_time = time.time()
+    while True:
+      try:
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        return
+      except (AssertionError, WebDriverException) as e:
+        if time.time() - start_time > MAX_WAIT:
+          raise e
+        time.sleep(.5)
 
   def test_can_start_a_list_and_retrieve_it_later(self):
     # Richard has heard about a new web app for To-Do lists. He goes
@@ -37,18 +48,18 @@ class NewVisitorTest(LiveServerTestCase):
     # When he hits enter, the item is updated in the To-Do list:
     # "1: Read and practice chapter 1 of TDD".
     input_box.send_keys(Keys.ENTER)
-    time.sleep(2)
-    self.check_for_row_in_list_table('1: Read and practice chapter 1 of TDD')
+    #time.sleep(2)
+    self.wait_for_row_in_list_table('1: Read and practice chapter 1 of TDD')
 
     # He sees he can enter another item so he enters:
     # "Create a new repository in GitHub and push"
     input_box = self.browser.find_element_by_id('id_new_item')
     input_box.send_keys('Create a new repository in GitHub and push')
     input_box.send_keys(Keys.ENTER)
-    time.sleep(2)
+    #time.sleep(2)
     # The page updates and shows the list
-    self.check_for_row_in_list_table('1: Read and practice chapter 1 of TDD')
-    self.check_for_row_in_list_table('2: Create a new repository in GitHub and push')
+    self.wait_for_row_in_list_table('1: Read and practice chapter 1 of TDD')
+    self.wait_for_row_in_list_table('2: Create a new repository in GitHub and push')
 
     # The page has generated a URL, Richard visits the URL and see his To-Do
     # list with 2 items.
